a55543cac019f8da6d0d11f48c6298aa2664d6a5
ark1111 <alireza.khaji11@gmail.com>
2023-11-18 23:35:52 +0330

    feat : ActionTip

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -7,6 +7,16 @@ export const Box = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #999;
+  position: relative;
 `;
 
-
+export const ActionTip = styled.div`
+  width: 200px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  font-size: 16px;
+  color: 14px;
+  padding: 10px;
+  border: 1px solid #111;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { Box } from "./App.styled";
+import { ActionTip, Box } from "./App.styled";
 import FilesExplorer from "./components/FilesExplorer";
 
 function App() {
   return (
     <Box>
       <FilesExplorer />
+      <ActionTip>
+        Right-click on a file or folder to delete or to add a new file or folder
+      </ActionTip>
     </Box>
   );
 }
